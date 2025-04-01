74d18f0c7c7468a6a29213a862f9f86e10dc441b
iammanas1975 <140784033+iammanas1975@users.noreply.github.com>
2025-04-02 00:00:44 +0530

    Comment out redundant line

--- trafficassign.py
+++ trafficassign.py
@@ -1,10 +1,9 @@
 #TRAFFIC LIGHT PROBLEM FROM 'DATA STRUCTURES AND ALGORITHMS' BY HOPCROFT, ET AL.
 #We consider 3 roads, say a, b, c; ab is a path from a to b, and so on
 #we have a graph with list of incompatible paths for a given path
-#we have to assign a color to each new path if incompatible with previous assigned ones
-#This is not a true greedy algorithm!
+#for path with incompatible paths, we assign a new color; else we assign the same color
 curr_color = 0 #color assignment
-ab = bc = ca = ba = cb = ac = 0 #initializing paths
+#ab = bc = ca = ba = cb = ac = 0 #initializing paths
 tcolor = {'ab':0, 'bc':0, 'ca':0, 'ba':0, 'cb':0, 'ac':0} #path to color assignments
 tgraph = {'ab':['ba','bc','ac'], 'bc':['ab','ca','ac'],'ca':['bc','ac'], \
           'ba':['ab'],'cb':[], 'ac':['ab','bc','ca']} #incompatible path list for a path
